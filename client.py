eb5733c282f6a839729cf32210f4cb309c75d336
Neumann Adam <adam.neumann@frends.com>
2024-05-04 01:55:01 +0200

    idk man... copied some code for multi-client connection :c

--- client.py
+++ client.py
@@ -1,11 +1,25 @@
+import sys
 import socket
+import selectors
+import types
 
-HOST = "127.0.0.1" 
-PORT = 65432 
+sel = selectors.DefaultSelector()
+messages = [b"Message 1 from client.", b"Message 2 from client."]
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-
-print(f"Received {data!r}")
+def start_connections(host, port, num_conns):
+    server_addr = (host, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print(f"Starting connection {connid} to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
+            messages=messages.copy(),
+            outb=b"",
+        )
+        sel.register(sock, events, data=data)
